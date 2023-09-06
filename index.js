3b5f5648244a6700b07fd9f0ead52a015f79e71a
majjigiprajwal <prajwal1majjigi@gmail.com>
2023-09-06 19:28:52 +0530

    express backend  connection

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const fetchProducts = async ()=>{
     try{
-      let  response = await axios.get('https://crudcrud.com/api/162fe5f7a4c74ac99c43522b0db5a82b/products')
-      var element = document.getElementById('products');
-      var totalPrice =0;
-      response.data.forEach((product)=>{
-        totalPrice=totalPrice+Number(product.price);
-        var item =  document.createElement('p');
-       item.textContent=`${product.product}-${product.price}`
+      let  response = await axios.get('http://localhost:7000/expenses')
+      var element = document.getElementById('expense-list');
+      response.data.forEach((expense)=>{
+        var item =  document.createElement('li');
+       item.textContent=`${expense.description}-${expense.category}-${expense.amount}RS`
+       item.classList="list-group-item list-group-item-danger"
        var deleteButton = document.createElement('button');
-       deleteButton.textContent="Delete Product";
-       deleteButton.id=`${product._id}`;
+       deleteButton.textContent="Delete Expense";
+       deleteButton.id=`${expense.id}`;
+       deleteButton.classList="deleteBtn"
        deleteButton.addEventListener('click',deleteProducts)
        item.appendChild(deleteButton);
        element.appendChild(item);
-       document.getElementById('price').textContent=totalPrice;
       })
     }
     catch(error){
@@ -22,72 +21,62 @@ const fetchProducts = async ()=>{
 }
 
 document.addEventListener('DOMContentLoaded',fetchProducts);
+
+
 const onSubmit= (e)=>{
     e.preventDefault();
-    let productName = document.getElementById('product').value;
-    let productPrice = document.getElementById('productPrice').value;
-    if(productName!=="" && productPrice!=""){
-        const product ={
-            product:productName,
-            price:productPrice,
-        }
-       AddProducts(product);
+    let description = document.getElementById('description').value;
+    let amount = document.getElementById('amount').value;
+    let category = document.getElementById('Category').value;
+
+    const expense ={
+        description:description,
+        amount:amount,
+        category:category
     }
-    else{
-        console.log('error occured');
-    }
+
+   AddProducts(expense);
+   
 }
 
-const AddToTotalPrice = (price)=>{
-     var priceElement = document.getElementById('price');
-     console.log(priceElement)
-     let currentTotal = priceElement.innerText;
-     console.log(currentTotal)
-     let totalPrice = Number(currentTotal) + Number(price);
-     priceElement.innerText=totalPrice;
-}
-
-const SubFromTotalPrice = (price)=>{
-    var priceElement = document.getElementById('price');
-    console.log(priceElement)
-    let currentTotal = priceElement.textContent;
-    console.log(currentTotal)
-    let totalPrice = parseInt(currentTotal)-parseInt(price);
-    priceElement.innerText=totalPrice;
-}
 
 const deleteProducts = async (e)=>{
     let id = e.target.id;
-    console.log(id)
      try{
-        let response = await axios.get(`https://crudcrud.com/api/162fe5f7a4c74ac99c43522b0db5a82b/products/${id}`);
-         await axios.delete(`https://crudcrud.com/api/162fe5f7a4c74ac99c43522b0db5a82b/products/${id}`);
-         var parent = document.getElementById(e.target.id).parentElement;
-         parent.remove();
-         SubFromTotalPrice(response.data.price)
+        let response =  await axios.post(`http://localhost:7000/expenses/${id}`);
+        if(response.status === 200){
+            var parent = document.getElementById(e.target.id).parentElement;
+            parent.remove();
+        }
+        else{
+            console.log('Cannot remove Expense')
+        }
      }
      catch(error){
          console.log(error);
      }
 }
 
-const AddProducts= async (product)=>{
+const AddProducts= async (expense)=>{
    try{
-       let response = await axios.post('https://crudcrud.com/api/162fe5f7a4c74ac99c43522b0db5a82b/products',product)
-       console.log(response);
-       var products = document.getElementById('products')
+       let response = await axios.post('http://localhost:7000/expenses',expense)
+       var expenses = document.getElementById('expense-list')
+       console.log(expenses);
 
-       var item =  document.createElement('p');
-       item.textContent=`${response.data.product}-${response.data.price}`
+       var item =  document.createElement('li');
+       item.textContent=`${response.data.description}-${response.data.category}-${response.data.amount}RS`
+       item.classList="list-group-item list-group-item-danger"
        var deleteButton = document.createElement('button');
        deleteButton.textContent="Delete Product";
-       deleteButton.id=`${response.data._id}`;
+       deleteButton.id=`${response.data.id}`;
+       deleteButton.classList="deleteBtn"
        deleteButton.addEventListener('click',deleteProducts)
        item.appendChild(deleteButton);
-       products.appendChild(item);
-       AddToTotalPrice(response.data.price);
-       document.getElementById('product').value="";
-       document.getElementById('productPrice').value="";
+       expenses.appendChild(item);
+      
+       document.getElementById('description').value="";
+       document.getElementById('amount').value="";
+       document.getElementById('Category').value="";
    }
    catch(error){
      console.log('error')
